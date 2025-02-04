2b95bb3f3e2f557aca2ff997f8b867127560ae02
Joao Correa <joaocln88@gmail.com>
2025-02-04 16:06:29 -0300

    feat: create and use `InternalServerError` in `/api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,31 +1,46 @@
 import database from "infra/database.js";
+import { InternalServerError } from "infra/errors";
 
 async function status(request, response) {
-  const updatedAt = new Date().toISOString();
+  try {
+    const updatedAt = new Date().toISOString();
 
-  let version = await database.query("SHOW server_version;");
-  version = version.rows[0].server_version;
+    let version = await database.query("SHOW server_version;");
+    version = version.rows[0].server_version;
 
-  let maxConnections = await database.query("SHOW max_connections;");
-  maxConnections = maxConnections.rows[0].max_connections;
+    let maxConnections = await database.query("SHOW max_connections;");
+    maxConnections = maxConnections.rows[0].max_connections;
 
-  const databaseName = process.env.POSTGRES_DB;
-  let usedConnections = await database.query({
-    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname=$1;",
-    values: [databaseName],
-  });
-  usedConnections = usedConnections.rows[0].count;
+    const databaseName = process.env.POSTGRES_DB;
+    let usedConnections = await database.query({
+      text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname=$1;",
+      values: [databaseName],
+    });
+    usedConnections = usedConnections.rows[0].count;
 
-  response.status(200).json({
-    updated_at: updatedAt,
-    dependencies: {
-      database: {
-        version: version,
-        max_connections: parseInt(maxConnections),
-        opened_connections: usedConnections,
+    response.status(200).json({
+      updated_at: updatedAt,
+      dependencies: {
+        database: {
+          version: version,
+          max_connections: parseInt(maxConnections),
+          opened_connections: usedConnections,
+        },
       },
-    },
-  });
+    });
+  } catch (error) {
+    const publicErrorObject = new InternalServerError({
+      cause: error,
+    });
+
+    console.error("\n Erro dentro do catch do controller:");
+    console.log(publicErrorObject);
+
+    response.status(500).json({
+      // error: "Internal Server Error",
+      publicErrorObject,
+    });
+  }
 }
 
 export default status;
